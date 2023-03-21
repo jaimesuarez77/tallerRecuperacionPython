43a5178cb1193dfd6caa47816a1e8bf07dadab82
jaimesuarez77 <jaimesuarez77@gmail.com>
2023-03-20 22:26:28 -0500

    actualizaciones

---
+++ ejercicio10.py
@@ -0,0 +1,22 @@
+
+
+    
+       
+while True:
+
+    num_uno=float(input("Ingrese un número: "))    
+
+    num_dos=float(input("Ingrese el segundo número: "))
+    total = num_uno + num_dos
+
+    print(total)
+
+    continuar = input("Desea continuar con mas sumas: 's' o 'n' ?")
+    if continuar == "n":
+        break
+    else:
+        True
+
+
+
+ 
